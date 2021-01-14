7d84d3b9bdce98a11b1493c69b6ac70526b62b41
BrenoLopes <brenolprimo@gmail.com>
2021-01-14 14:46:45 -0400

    Added: an exception to return a view with conflict httpstatus code

---
+++ backend/src/main/java/br/balladesh/pjcappbackend/utilities/errors/ConflictException.java
@@ -0,0 +1,25 @@
+package br.balladesh.pjcappbackend.utilities.errors;
+
+import org.springframework.http.HttpStatus;
+
+public class ConflictException extends HttpException {
+  public ConflictException() {
+    super(HttpStatus.CONFLICT);
+  }
+
+  public ConflictException(String message) {
+    super(message, HttpStatus.CONFLICT);
+  }
+
+  public ConflictException(String message, Throwable cause) {
+    super(message, cause, HttpStatus.CONFLICT);
+  }
+
+  public ConflictException(Throwable cause) {
+    super(cause, HttpStatus.CONFLICT);
+  }
+
+  public ConflictException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace, HttpStatus.CONFLICT);
+  }
+}
