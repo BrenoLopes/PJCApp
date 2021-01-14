6712cae58e95b468dc7d15851990f3af2f6c42b3
BrenoLopes <brenolprimo@gmail.com>
2021-01-14 14:43:22 -0400

    Updated: Refactored from builder to factory pattern

--- backend/src/main/java/br/balladesh/pjcappbackend/utilities/builders/Builder.java
+++
@@ -1,7 +0,0 @@
-package br.balladesh.pjcappbackend.utilities.builders;
-
-import br.balladesh.pjcappbackend.utilities.Result;
-
-public interface Builder<T, U extends Exception> {
-  Result<T, U> build();
-}
