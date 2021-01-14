1a258b8936ecfcdfcfb9f66b9ee130b3719b7b21
BrenoLopes <brenolprimo@gmail.com>
2021-01-14 14:45:47 -0400

    Updated: Included a method for finding an album by the name

--- backend/src/main/java/br/balladesh/pjcappbackend/repository/AlbumRepository.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/repository/AlbumRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface AlbumRepository extends PagingAndSortingRepository<AlbumEntity, Long> {
   @Query("select a from Albums a join a.artist a2 " +
       "where (:id is null or a.id in :id) " +
       "and (:name is null or a.name in :name) " +
       "and (:artistName is null or a2.name in :artistName)")
   Page<AlbumEntity> findByIdAndNameAndArtist_Name(Long id, String name, String artistName, Pageable pageable);
+
+  Optional<AlbumEntity> findByName(String name);
 }
