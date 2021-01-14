15c885531fc18bbee616e1608e7772c9d5a3a26f
BrenoLopes <brenolprimo@gmail.com>
2021-01-13 22:08:13 -0400

    Added: Controller and DTO for displaying a list of every album, with support for pagination.

---
+++ backend/src/main/java/br/balladesh/pjcappbackend/controllers/api/albums/GetAllAlbumsController.java
@@ -0,0 +1,54 @@
+package br.balladesh.pjcappbackend.controllers.api.albums;
+
+import br.balladesh.pjcappbackend.controllers.BuildResponseFromException;
+import br.balladesh.pjcappbackend.dto.api.albums.AllAlbumResponse;
+import br.balladesh.pjcappbackend.entity.AlbumEntity;
+import br.balladesh.pjcappbackend.repository.AlbumRepository;
+import br.balladesh.pjcappbackend.utilities.errors.InternalServerErrorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/albums")
+public class GetAllAlbumsController {
+  private final AlbumRepository albumRepository;
+  private final HttpHeaders json_content_header = new HttpHeaders();
+
+  private final Logger logger = LoggerFactory.getLogger(GetAllAlbumsController.class);
+
+  @Autowired
+  public GetAllAlbumsController(AlbumRepository albumRepository)
+  {
+    this.albumRepository = albumRepository;
+    this.json_content_header.add("Content-Type", "application/json");
+  }
+
+  @GetMapping("/list")
+  public ResponseEntity<?> getAllAlbums(
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "10") int pagesize
+  ){
+    try {
+      Pageable _page = PageRequest.of(page, pagesize);
+      Page<AlbumEntity> albumPages = this.albumRepository.findAll(_page);
+
+      return ResponseEntity.ok(new AllAlbumResponse(albumPages));
+    } catch(Exception e) {
+      this.logger.error(
+          "GetAllAlbumsController::getAllAlbums Could not process the request because of an error! Error: {}",
+          e.getMessage()
+      );
+
+      return new BuildResponseFromException(
+          new InternalServerErrorException("An error happened in the server! Please try again latter!")
+      ).build().getData();
+    }
+  }
+}
---
+++ backend/src/main/java/br/balladesh/pjcappbackend/dto/api/albums/AllAlbumResponse.java
@@ -0,0 +1,75 @@
+package br.balladesh.pjcappbackend.dto.api.albums;
+
+import br.balladesh.pjcappbackend.entity.AlbumEntity;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Objects;
+
+public class AllAlbumResponse {
+  public final List<AlbumEntity> albums;
+  public final int currentPage, totalPages;
+  public final long totalItems;
+
+  public AllAlbumResponse(List<AlbumEntity> albums, int currentPage, long totalItems, int totalPages) {
+    this.albums = albums;
+    this.currentPage = currentPage;
+    this.totalPages = totalPages;
+    this.totalItems = totalItems;
+  }
+
+  public AllAlbumResponse(Page<AlbumEntity> pagedAlbumEntity) {
+    this.albums = pagedAlbumEntity.getContent();
+    this.currentPage = pagedAlbumEntity.getNumber();
+    this.totalItems = pagedAlbumEntity.getTotalElements();
+    this.totalPages = pagedAlbumEntity.getTotalPages();
+  }
+
+  public List<AlbumEntity> getAlbums() {
+    return albums;
+  }
+
+  public int getCurrentPage() {
+    return currentPage;
+  }
+
+  public int getTotalPages() {
+    return totalPages;
+  }
+
+  public long getTotalItems() {
+    return totalItems;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    AllAlbumResponse that = (AllAlbumResponse) o;
+    return currentPage == that.currentPage && totalPages == that.totalPages && totalItems == that.totalItems && albums.equals(that.albums);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(albums, currentPage, totalPages, totalItems);
+  }
+
+  @Override
+  public String toString() {
+    String str
+        = "{"
+        + "albums=%s,"
+        + "currentPage=\"%d\","
+        + "totalItems=\"%d\","
+        + "totalPages=\"%d\""
+        + "}";
+
+    return String.format(
+        str,
+        this.albums,
+        this.currentPage,
+        this.totalItems,
+        this.totalPages
+    );
+  }
+}
