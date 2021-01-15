a3ec72ca2fab99572e21b6571a12a686875dd041
BrenoLopes <brenolprimo@gmail.com>
2021-01-15 00:23:44 -0400

    Added: A command to get a file's url with expiration from MinIO or an empty url for invalid files

---
+++ backend/src/main/java/br/balladesh/pjcappbackend/minio/GetFromMinIOCommand.java
@@ -0,0 +1,77 @@
+package br.balladesh.pjcappbackend.minio;
+
+import br.balladesh.pjcappbackend.services.MinIOEndpoint;
+import br.balladesh.pjcappbackend.utilities.Result;
+import br.balladesh.pjcappbackend.utilities.commands.Command;
+import br.balladesh.pjcappbackend.utilities.errors.HttpException;
+import io.minio.BucketExistsArgs;
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.MinioClient;
+import io.minio.StatObjectArgs;
+import io.minio.http.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class GetFromMinIOCommand implements Command<String, HttpException> {
+  private final String file;
+  private final MinIOEndpoint endpoint;
+  private final Logger logger = LoggerFactory.getLogger(UploadToMinIOCommand.class);
+
+  public GetFromMinIOCommand(String file, MinIOEndpoint endpoint) {
+    this.file = file == null ? "" : file;
+    this.endpoint = endpoint;
+  }
+
+  @Override
+  public Result<String, HttpException> execute() {
+    try {
+      if (this.file.equals(""))
+        return this.returnEmptyUrl();
+
+      MinioClient minioClient = MinioClient.builder()
+          .endpoint(this.endpoint.getEndpoint())
+          .credentials(this.endpoint.getAccessKey(), this.endpoint.getSecretKey())
+          .build();
+
+      boolean found = minioClient
+          .bucketExists(
+              BucketExistsArgs.builder().bucket(this.endpoint.getBucketName()).build()
+          );
+
+      Object object = minioClient.statObject(
+          StatObjectArgs.builder()
+              .bucket(this.endpoint.getBucketName())
+              .object(this.file)
+              .build()
+      );
+
+      if (!found && object != null)
+        return Result.from("");
+
+      String url =
+          minioClient.getPresignedObjectUrl(
+              GetPresignedObjectUrlArgs.builder()
+                  .method(Method.GET)
+                  .bucket(this.endpoint.getBucketName())
+                  .object(this.file)
+                  .expiry(5, TimeUnit.MINUTES)
+                  .build());
+
+      return Result.from(url);
+    } catch (Exception e) {
+      logger.error(
+          "GetFromMinIOCommand::execute Could not load the url for object {}. Error: {}",
+          this.file,
+          e.getMessage()
+      );
+    }
+
+    return this.returnEmptyUrl();
+  }
+
+  private Result<String, HttpException> returnEmptyUrl() {
+    return Result.from("");
+  }
+}
