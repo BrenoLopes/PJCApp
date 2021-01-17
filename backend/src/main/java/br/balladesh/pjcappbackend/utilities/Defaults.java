2cb4a8bb514f03e4c97458d20ce319c8fca9fc88
BrenoLopes <brenolprimo@gmail.com>
2021-01-17 13:24:00 -0400

    Added: A class to map default request values for future refactoring

---
+++ backend/src/main/java/br/balladesh/pjcappbackend/utilities/Defaults.java
@@ -0,0 +1,9 @@
+package br.balladesh.pjcappbackend.utilities;
+
+public class Defaults {
+  public static final String DEFAULT_BOOLEAN = "" + false;
+  public static final String DEFAULT_LONG    = "" + Long.MIN_VALUE;
+  public static final String DEFAULT_INT     = "" + Integer.MIN_VALUE;
+  public static final String DEFAULT_STR     = "@Default" + 646890207; // Hash from "@THIS_IS_DEFAULT_STR".hashCode();
+
+}
