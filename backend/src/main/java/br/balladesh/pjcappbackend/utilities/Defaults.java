eb960ea01263909d4c7ada6adeeb9e7b1f151459
BrenoLopes <brenolprimo@gmail.com>
2021-01-18 18:46:36 -0400

    Updated: Included shortcuts for the long and int default values, instead of getting a str

--- backend/src/main/java/br/balladesh/pjcappbackend/utilities/Defaults.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/utilities/Defaults.java
@@ -6,4 +6,11 @@ public class Defaults {
   public static final String DEFAULT_INT     = "" + Integer.MIN_VALUE;
   public static final String DEFAULT_STR     = "@Default" + 646890207; // Hash from "@THIS_IS_DEFAULT_STR".hashCode();
 
+  public static long getDefaultLong() {
+    return Long.parseLong(DEFAULT_LONG);
+  }
+
+  public static int getDefaultInt() {
+    return Integer.parseInt(DEFAULT_INT);
+  }
 }
