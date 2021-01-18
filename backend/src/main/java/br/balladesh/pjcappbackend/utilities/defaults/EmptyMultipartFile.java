7776870e5deca2047da768078fb29f3c5698bf9b
BrenoLopes <brenolprimo@gmail.com>
2021-01-18 14:32:34 -0400

    Updated: Implemented equals and hash code.

--- backend/src/main/java/br/balladesh/pjcappbackend/utilities/defaults/EmptyMultipartFile.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/utilities/defaults/EmptyMultipartFile.java
@@ -1,5 +1,7 @@
 package br.balladesh.pjcappbackend.utilities.defaults;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
@@ -34,15 +36,40 @@ public final class EmptyMultipartFile implements MultipartFile {
   }
 
   @Override
-  public byte[] getBytes() throws IOException {
+  public byte[] getBytes() {
     return new byte[0];
   }
 
   @Override
-  public InputStream getInputStream() throws IOException {
+  public InputStream getInputStream() {
     return new ByteArrayInputStream(this.getBytes());
   }
 
   @Override
-  public void transferTo(File file) throws IOException, IllegalStateException {}
+  public void transferTo(File file) {}
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null) return false;
+    if (o.getClass() != this.getClass()) return false;
+
+    final EmptyMultipartFile that = (EmptyMultipartFile) o;
+    return Objects.equal(this.getBytes(), that.getBytes())
+        && Objects.equal(this.getName(), that.getName())
+        && Objects.equal(this.getContentType(), that.getName())
+        && Objects.equal(this.isEmpty(), that.isEmpty())
+        && Objects.equal(this.getSize(), that.getSize())
+        && Objects.equal(this.getOriginalFilename(), this.getOriginalFilename());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(
+        this.getBytes(),
+        this.getName(),
+        this.getContentType(),
+        this.getOriginalFilename(),
+        this.getSize()
+    );
+  }
 }
