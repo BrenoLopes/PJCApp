775734ba77b243657cc5d221bebe2469adfcd856
BrenoLopes <brenolprimo@gmail.com>
2021-01-18 22:27:15 -0400

    Updated: Added a shortcut method to get the default boolean

--- backend/src/main/java/br/balladesh/pjcappbackend/utilities/Defaults.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/utilities/Defaults.java
@@ -4,7 +4,7 @@ public class Defaults {
   public static final String DEFAULT_BOOLEAN = "" + false;
   public static final String DEFAULT_LONG    = "" + Long.MIN_VALUE;
   public static final String DEFAULT_INT     = "" + Integer.MIN_VALUE;
-  public static final String DEFAULT_STR     = "@Default" + 646890207; // Hash from "@THIS_IS_DEFAULT_STR".hashCode();
+  public static final String DEFAULT_STR     = "";
 
   public static long getDefaultLong() {
     return Long.parseLong(DEFAULT_LONG);
@@ -12,5 +12,9 @@ public class Defaults {
 
   public static int getDefaultInt() {
     return Integer.parseInt(DEFAULT_INT);
+  }
+
+  public static boolean getDefaultBoolean() {
+    return Boolean.parseBoolean(DEFAULT_BOOLEAN);
   }
 }
