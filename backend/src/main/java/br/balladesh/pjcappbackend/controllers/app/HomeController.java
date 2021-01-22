2463cd55569b3cfe79d4f08b2d4d86688ee7129c
BrenoLopes <brenolprimo@gmail.com>
2021-01-22 16:13:33 -0400

    Updated: Included support to serve an static html file from the server itself, preparing for the frontend to be release with the jar.

---
+++ backend/src/main/java/br/balladesh/pjcappbackend/controllers/app/HomeController.java
@@ -0,0 +1,12 @@
+package br.balladesh.pjcappbackend.controllers.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+  @RequestMapping(value = "/", produces = "text/html")
+  public String home() {
+    return "index";
+  }
+}
