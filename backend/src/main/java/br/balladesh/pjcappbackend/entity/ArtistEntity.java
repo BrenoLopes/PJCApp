b54aabf686e19b9ba1a820a661c37fa3a40ce555
BrenoLopes <brenolprimo@gmail.com>
2021-01-22 14:11:07 -0400

    Updated: Included a Cascade operation when removing at the foreign table mapping, because if not, it'll fail to delete an artist with albums.

--- backend/src/main/java/br/balladesh/pjcappbackend/entity/ArtistEntity.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/entity/ArtistEntity.java
@@ -20,7 +20,7 @@ public class ArtistEntity {
   @Column(nullable = false, unique = true)
   private String name;
 
-  @OneToMany(mappedBy = "artist")
+  @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "artist")
   private final List<AlbumEntity> albums;
 
   public ArtistEntity() {
