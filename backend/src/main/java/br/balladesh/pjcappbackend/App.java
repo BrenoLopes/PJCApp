6cc4d610afd272113f97e2a2adc408bf8267f5a7
BrenoLopes <brenolprimo@gmail.com>
2021-01-25 15:51:21 -0400

    Updated: Moved rerouter into app's controller

--- backend/src/main/java/br/balladesh/pjcappbackend/App.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/App.java
@@ -2,19 +2,10 @@ package br.balladesh.pjcappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-	}
-
-	@RequestMapping(value = "/**/{path:[^.]*}")
-	public String redirect() {
-		return "forward:/";
 	}
 }
