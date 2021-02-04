89ed53691f3a5b39f5d4bc9bbd641d573a6d9524
BrenoLopes <brenolprimo@gmail.com>
2021-02-04 09:27:44 -0400

    Added: A component to allow the user to log into the server

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import {
+  LoginRequest,
+  LoginResponse,
+  LoginService,
+} from '@core/services/auth/login/login.service';
+import { MyStorageService } from '@core/services/storage/my-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private snackBar: MatSnackBar,
+    private localStorage: MyStorageService
   ) {}
 
   ngOnInit(): void {
@@ -23,9 +40,8 @@ export class LoginComponent implements OnInit {
   onSubmit(): void {
     this.loginForm.disable();
 
-    console.log(`You have tried to log in with ${this.getEmail()?.value} and ${this.getPassword()?.value}`);
-
-    this.loginForm.enable();
+    const runWhenRequestEnds = () => this.loginForm.enable();
+    this.doTheRequest(runWhenRequestEnds);
   }
 
   getEmail(): AbstractControl | null {
@@ -34,5 +50,42 @@ export class LoginComponent implements OnInit {
 
   getPassword(): AbstractControl | null {
     return this.loginForm.get('password');
+  }
+
+  private doTheRequest(runAfter: () => void): void {
+    const request: LoginRequest = {
+      username: this.getEmail()?.value,
+      password: this.getPassword()?.value,
+    };
+
+    const onSuccessfulSignUp = (response: LoginResponse) => {
+      this.snackBar.open('O login foi feito com sucesso!');
+
+      this.localStorage.setJwtToken(response.token);
+      this.localStorage.setUsername(response.username);
+
+      console.log(this.localStorage.getUsername(), this.localStorage.getJwtToken());
+
+      runAfter();
+    };
+
+    const onFailedSignUp = (error: HttpErrorResponse) => {
+      const message: string = this.createMessageFromError(error);
+
+      this.snackBar.open(message, 'Ok');
+      runAfter();
+    };
+
+    this.loginService
+      .requestLogin(request)
+      .subscribe(onSuccessfulSignUp, onFailedSignUp);
+  }
+
+  private createMessageFromError(error: HttpErrorResponse): string {
+    if (error.status === 401) {
+      return 'Suas credenciais estão incorretas!';
+    }
+
+    return 'Whoops, algo de errado aconteceu no servidor!';
   }
 }
