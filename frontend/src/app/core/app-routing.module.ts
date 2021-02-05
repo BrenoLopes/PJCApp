294a83a9c3ee6ad25a81efb993547aa0e91a7919
BrenoLopes <brenolprimo@gmail.com>
2021-02-05 09:50:54 -0400

    Updated: Added a route to display a list of albums

--- frontend/src/app/core/app-routing.module.ts
+++ frontend/src/app/core/app-routing.module.ts
@@ -5,9 +5,11 @@ import { HomeComponent } from '@pages/home/home.component';
 import { LoginComponent } from '@pages/login/login.component';
 import { SignupComponent } from '@pages/signup/signup.component';
 import { NotfoundComponent } from '@pages/notfound/notfound.component';
+import { ListAllAlbumsComponent } from '@pages/albums/list-all/list-all-albums.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'artist/:artist', component: ListAllAlbumsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cadastrar', component: SignupComponent },
   { path: 'nao_encontrado', component: NotfoundComponent },
