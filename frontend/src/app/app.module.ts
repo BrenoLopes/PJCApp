d775538574156b3f72da112bac245c4600e568c6
BrenoLopes <brenolprimo@gmail.com>
2021-02-04 22:43:26 -0400

    Updated: Included the shared module into the app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { PagesModule } from '@pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     CoreModule,
     PagesModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
