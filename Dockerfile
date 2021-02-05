7bc9a00e7691ea31940a94c7faa446fee5555653
BrenoLopes <brenolprimo@gmail.com>
2021-02-05 11:17:10 -0400

    Added: Static Files

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:8u275-jdk
+
+WORKDIR /app
+COPY . /app/
+
+EXPOSE 8080
--- backend/src/main/java/br/balladesh/pjcappbackend/controllers/app/HomeController.java
+++ backend/src/main/java/br/balladesh/pjcappbackend/controllers/app/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-  @RequestMapping(value = "/", produces = "text/html")
-  public String home() {
-    return "index";
-  }
+//  @RequestMapping(value = "/", produces = "text/html")
+//  public String home() {
+//    return "index";
+//  }
 
   @RequestMapping(value = "/**/{path:[^.]*}")
   public String redirect() {
